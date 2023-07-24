deb11aaaaafe964b9ca4cf1c3bbbb08d48c8ce89
Sorikairo <sorikairox@gmail.com>
2023-07-24 23:48:12 +0900

    feat: change X logo to twitter logo

--- content.js
+++ content.js
@@ -1,12 +1,18 @@
-function faviconURL(u) {
-  const url = new URL(chrome.runtime.getURL('/_favicon/'));
-  url.searchParams.set('pageUrl', u); // this encodes the URL as well
-  url.searchParams.set('size', '32');
-  return url.toString();
-}
+var logoHasBeenModified = false;
+var observer = new MutationObserver(function (mutations) {
+  mutations.forEach(function (mutation) {
+    if (!mutation.addedNodes || logoHasBeenModified) {
+      return;
+    }
+    const element = document.querySelector('[aria-label="Twitter"]');
+    if (element) {
+      logoHasBeenModified = true;
+      element.children[0].innerHTML = `<img class='x-to-twitter-logo' src="https://github.com/Sorikairox/twitter-extension/blob/main/Logo_of_Twitter.svg.png?raw=true">`
+    }
+  });
+});
 
-const imageOverlay = document.createElement('img');
-imageOverlay.src = faviconURL('https://www.google.com');
-imageOverlay.alt = "Google's favicon";
-imageOverlay.classList.add('favicon-overlay');
-document.body.appendChild(imageOverlay);
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+});
